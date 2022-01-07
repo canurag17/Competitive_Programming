38850b35b756d4abf18f5d32cef83ac9d1e792d0
Anurag Chowdhury <77167540+canurag17@users.noreply.github.com>
2022-01-07 14:20:03 +0530

    Time: 19 ms (29.36%), Space: 5.8 MB (90.95%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -3,10 +3,16 @@ public:
     bool isPalindrome(int x) {
         if(x<0)
             return false;
-        string s=to_string(x);
-        string s2=to_string(x);
-        reverse(s.begin(),s.end());
-        return s==s2;
-        
+        long long int t=x;
+        long long int y=x;
+        long long int rev=0;
+        long long int r;
+        while(t>0)
+        {
+            r=t%10;
+            t=t/10;
+            rev=rev*10+r;
+        }
+        return y==rev;
     }
 };
