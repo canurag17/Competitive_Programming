a86c38a91311f843144d26a2db6a79edace284c0
Anurag Chowdhury <77167540+canurag17@users.noreply.github.com>
2022-03-07 23:35:12 +0530

    Time: 416 ms (11.19%), Space: 34.3 MB (56.59%) - LeetHub

---
+++ 87-scramble-string/87-scramble-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    map<string,bool> mp;
+    bool isScramble(string s1, string s2) {
+        int n=s1.length();
+        if(s1.length()==0 || s2.length()==0)
+            return false;
+        if(s1==s2)
+            return true;
+        string s;
+        s.append(s1);
+        s.push_back('_');
+        s.append(s2);
+        if(mp.find(s)!=mp.end())
+            return mp[s];
+        for(int i=1;i<s1.length();i++)
+        {
+            bool c1=isScramble(s1.substr(0,i),s2.substr(n-i,i)) && isScramble(s1.substr(i,n-i),s2.substr(0,n-i));
+            bool c2=isScramble(s1.substr(0,i),s2.substr(0,i)) && isScramble(s1.substr(i,n-i),s2.substr(i,n-i));
+            if(c1 || c2)
+                return mp[s]=true;
+        }
+        return mp[s]=false;
+    }
+};
