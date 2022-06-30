1aacbd916aa133612f7b016d36e6328c342b12ab
Anurag Chowdhury <77167540+canurag17@users.noreply.github.com>
2022-06-30 20:36:20 +0530

    Time: 57 ms (18.66%), Space: 13.5 MB (12.87%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        set<char> st;
+        int i=0, j=0;
+        int mx=0;
+        while(j<s.length())
+        {
+            st.insert(s[j]);
+            if(j-i+1==st.size())
+            {
+                mx=max(mx,j-i+1);
+                j++;
+            }
+            else if(j-i+1>st.size())
+            {
+                while(j-i+1>st.size())
+                {
+                    if(st.find(s[i])!=st.end())
+                        st.erase(s[i]);
+                    i++;
+                    st.insert(s[j]);
+                }
+                mx=max(mx,j-i+1);
+                j++;
+            }
+                
+        }
+        return mx;
+    }
+};
