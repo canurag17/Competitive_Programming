33c032f9b453d7ff949086f17627d0504f3e3324
Anurag Chowdhury <canurag18212@gmail.com>
2022-07-12 22:39:20 +0530

    806

---
+++ Codeforces/806/2.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<vector<int>>
+#define vll vector<vector<ll>>
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pdd pair<double,double>
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define forn(i,n) for(int i=0;i<n;i++)
+#define forab(i,a,b) for(int i=a;i<=b;i++)
+#define pqi priority_queue<int>
+#define pqd priority_queue<int,vector<int>,greater<int>>
+#define NO cout<<"NO"<<"\n"
+#define YES cout<<"YES"<<"\n"
+#define MOD 1000000007
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int tot=0;
+        map<char,int> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]]++;
+            if(mp[s[i]]==1)
+            tot+=2;
+            else
+            tot+=1;
+        }
+        cout<<tot<<"\n";
+
+    }
+    return 0;
+}
