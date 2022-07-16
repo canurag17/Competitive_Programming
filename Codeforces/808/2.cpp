703027e15621dc384782d07850c8340f62d315a3
Anurag Chowdhury <canurag18212@gmail.com>
2022-07-16 22:52:03 +0530

    808

---
+++ Codeforces/808/2.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<vector<int>>
+#define vll vector<vector<ll>>
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pdd pair<double,double>
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define forn(i,n) for(int i=0;i<n;i++)
+#define forab(i,a,b) for(int i=a;i<=b;i++)
+#define pqi priority_queue<int>
+#define pqd priority_queue<int,vector<int>,greater<int>>
+#define NO cout<<"NO"<<"\n"
+#define YES cout<<"YES"<<"\n"
+#define MOD 1000000007
+void solve()
+{
+    int n,l,r;
+    cin>>n>>l>>r;
+    int a[n+1];
+    for(int i=1;i<=n;i++)
+    {
+        if(l%i==0)
+        a[i]=l;
+        else
+        {
+            int t=l+(i-l%i);
+            if(t>r)
+            {
+                NO;
+                return;
+            }
+            a[i]=t;
+        }
+    }
+    YES;
+    forab(i,1,n)
+    cout<<a[i]<<" ";
+    cout<<"\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
