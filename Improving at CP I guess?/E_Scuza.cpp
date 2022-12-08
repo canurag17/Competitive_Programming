4ff644a6142f9e7fd3c7935bdccc60aaa739e818
Anurag Chowdhury <canurag18212@gmail.com>
2022-12-09 02:46:11 +0530

    Day #4 not that great a daybut ok

---
+++ Improving at CP I guess?/E_Scuza.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<vector<int>>
+#define vll vector<vector<ll>>
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pdd pair<double,double>
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define forn(i,n) for(int i=0;i<n;i++)
+#define forab(i,a,b) for(int i=a;i<=b;i++)
+#define pqi priority_queue<int>
+#define pqd priority_queue<int,vector<int>,greater<int>>
+#define NO cout<<"NO"<<"\n"
+#define YES cout<<"YES"<<"\n"
+#define MOD 1000000007
+void solve()
+{
+    ll n,q;
+    cin>>n>>q;
+    ll a[n+1];
+    ll k[q+1];
+    vector<pll> v;
+    forab(i,1,n)
+    {
+        cin>>a[i];
+    }
+    forab(i,1,q)
+    {
+        cin>>k[i];
+        v.pb({k[i],i});
+    }
+    sort(v.begin(),v.end());
+    int j=1;
+    ll u=0;
+    for(auto it: v)
+    {
+        while(j<=n)
+        {
+            if(it.first>=a[j])
+            {
+                u+=a[j];
+                j++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        k[it.second]=u;
+    }
+    forab(i,1,q)
+    cout<<k[i]<<" ";
+    cout<<"\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
