b247a36973a3f3d75880afed3b6efb8fd79b253f
Anurag Chowdhury <canurag18212@gmail.com>
2023-01-09 08:21:12 +0530

    lis rev lis

---
+++ Improving at CP I guess?/C_LIS_or_Reverse_LIS.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<vector<int>>
+#define vll vector<vector<ll>>
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pdd pair<double,double>
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define forn(i,n) for(int i=0;i<n;i++)
+#define forab(i,a,b) for(int i=a;i<=b;i++)
+#define revs(n) for(int i=n-1;i>=0;i--)
+#define pql priority_queue<ll>
+#define pqd priority_queue<pll,vector<pll>,greater<pll>>
+#define NO cout<<"NO"<<"\n"
+#define YES cout<<"YES"<<"\n"
+#define read(a) cin>>a
+#define wrt(a) cout<<a<<"\n"
+#define wrts(a) cout<<a<<" "
+#define MOD 1000000007
+void solve()
+{
+    ll n;
+    cin>>n;
+    ll a[n];
+    map<ll,ll> ctr;
+    forn(i,n)
+    {
+        cin>>a[i];
+        ctr[a[i]]++;
+    }
+    ll t=0,s=0;
+    for(auto it: ctr)
+    {
+        if(it.second>1)
+        {
+            t++;
+        }
+        else
+        {
+            s++;
+        }
+    }
+    cout<<t+(s+1)/2<<'\n';
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
